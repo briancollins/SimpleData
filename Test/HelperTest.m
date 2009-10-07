0104019a77e8d9b10e5d1abccac3743dafa79b49
Brian Collins <brian@livecurrent.com>
2009-10-06 20:48:28 -0700

    helper test for array shift

--- Test/HelperTest.m
+++ Test/HelperTest.m
@@ -10,17 +10,26 @@
 #import "NSString.h"
 #import "NSObject.h"
 #import "Employee.h"
+#import "NSMutableArray.h"
 
 @implementation HelperTest
 
-- (void) testUncapitalizedString {
+- (void)testUncapitalizedString {
     STAssertEqualStrings(@"helloWorld", [@"HelloWorld" uncapitalizedString], 
 						 @"String should be uncapitalized but camel case preserved");
 }
 
-- (void) testDescribeProperties {
+- (void)testDescribeProperties {
 	STAssertEqualStrings(@"NSString", [[Employee describeProperties] objectForKey:@"name"], 
 						 @"The type of the name property should be an NSString");
 }
+
+- (void)testMutableArrayShift {
+	NSMutableArray *a = [NSMutableArray arrayWithObjects:@"a", @"b", @"c", nil];
+	STAssertEqualStrings(@"a", [a shift], @"unshift should remove and return first element");
+	STAssertEqualStrings(@"b", [a shift], @"unshift should remove and return first element");
+	STAssertEqualStrings(@"c", [a shift], @"unshift should remove and return first element");
+}
+
 
 @end
